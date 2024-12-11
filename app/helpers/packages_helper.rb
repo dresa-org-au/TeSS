058ce0d12a4a82cf9b563868fffd8c9c9da1be1d
DReSA Support <91715679+dresa-org-au@users.noreply.github.com>
2024-12-11 17:10:13 +1000

    Rename packages (#100)
    * fix show bugs
    
    * update-faqs
    
    * update route
    
    * rename packages into my DReSA Link
    
    ---------
    
    Co-authored-by: root <u6712879@anu.edu.au>

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,7 +1,7 @@
 # The helper for Packages classes
 module PackagesHelper
 
-  PACKAGES_INFO = "Packages can be thought of as folders in which users may collect particular training materials or " +
+  PACKAGES_INFO = "My DReSA Links can be thought of as folders in which users may collect particular training materials or " +
     "events, from the full catalogue available within #{TeSS::Config.site['title_short']}, " +
     "to address their specific training needs."
 
